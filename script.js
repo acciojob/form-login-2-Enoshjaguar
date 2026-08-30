a49d7a8b4057bb6602427658c05a8147f0b52ff5
Enoshjaguar <dasarienosh013@gmail.com>
2026-08-30 23:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function submit(){
 let lname = document.getElementById('Last Name').value
 let mobile = document.getElementById('Phone Number').value
 let mail = document.getElementById('Email ID').value
-	alert(`Fisrt Name: ${fname} Last Name: ${lname} Phone Number: ${mobile} Email ID: ${mail}`)
+	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${mobile} Email ID: ${mail}`)
 }
