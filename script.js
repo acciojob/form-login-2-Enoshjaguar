d60fdb45f6154ba378c1fe4dada028153d6d4bce
Enoshjaguar <dasarienosh013@gmail.com>
2026-08-30 23:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+
+function submit(){
+	let fname = document.getElementById('fname').value
+let lname = document.getElementById('lname').value
+let mobile = document.getElementById('mobile').value
+let mail = document.getElementById('mail').value
+	alert(`Fisrt Name: ${fname} Last Name: ${lname} Phone Number: ${mobile} Email ID: ${mail}`)
+}
